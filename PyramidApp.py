889867f189be9bbf26b5ec30772a13fe52304094
artofmind <nancytaranenko@yandex.ru>
2016-11-11 21:36:26 +0500

    Create PyramidApp.py

---
+++ PyramidApp.py
@@ -0,0 +1,23 @@
+from wsgiref.simple_server import make_server #Импортирует функцию make_server, которая может создать простой веб-сервер при передаче приложения
+from pyramid.response import FileResponse
+from pyramid.config import Configurator
+
+environment=Environment(loader=FileSystemLoader('Templates'))
+il = '<a href="/index.html">Index</a>'
+al = '<a href="/about/aboutme.html">About me</a>'
+    
+def Index(request):
+    return Response(environment.get_template('/index.html').render(link=al, title='Index', text='<h1>Index</h1>'))
+    
+def AboutMe(request):    
+    return Response(environment.get_template('/about/aboutme.html').render(link=il, title='AboutMe' text='<h1>Aboutme</h1>'))
+    
+if __name__ == "__main__":
+    config = Configurator()
+    config.add_view(Index, route_name="index")
+    config.add_route("/index.html", "index")
+    config.add_view(AboutMe, route_name="about_me")
+    config.add_route("about_me", "/about/aboutme.html")
+    
+#запустить сервер
+make_server(config.make_wsgi_app(), host='localhost', port=8000).serve_forever()   
